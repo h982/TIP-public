09c58985ca69e7e6aabe37e7b30f713dc3615b92
h982 <ericharry@naver.com>
2021-11-28 21:42:21 +0900

    fix: id 문제 해결
    2021.11.08
    - id 가 0부터 시작하면 js 에서 Server 로 보낼 때 ''를 붙이는 문제
    - id 시작이 0이 아니게 만듦

--- backend/src/main/java/com/web/tip/util/IdGenerator.java
+++ backend/src/main/java/com/web/tip/util/IdGenerator.java
@@ -7,7 +7,9 @@ public class IdGenerator {
 
     public String generateId() {
         StringBuilder id = new StringBuilder();
-        for (int i = 0; i < 13; i++) {
+        // id 가 0부터 시작하면 js 에서 server 로 id 전달시 ''추가하는 오류 발생
+        id.append(random.nextInt(9) + 1);
+        for (int i = 1; i < 13; i++) {
             id.append(random.nextInt(10));
         }
         return id.toString();
